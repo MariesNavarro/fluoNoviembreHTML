11947740cb6fc24dfd7ac8f94eee182cedd6880a
Mn <fluo@Fluos-iMac.local>
2017-11-27 13:52:38 -0600

    Slider detect swipe

--- js/main.js
+++ js/main.js
@@ -359,6 +359,62 @@ function detectWaveLayouts(){
   }
 }
 
+/* wr, fun */
+function detectswipe(el,func) {
+  let swipe_det = new Object();
+  swipe_det.sX = 0;
+  swipe_det.sY = 0;
+  swipe_det.eX = 0;
+  swipe_det.eY = 0;
+  var min_x = 20;
+  var max_x = 40;
+  var min_y = 40;
+  var max_y = 50;
+  var direc = "";
+  let ele = document.getElementById(el);
+  ele.addEventListener('touchstart',function(e){
+  var t = e.touches[0];
+  swipe_det.sX = t.screenX;
+  swipe_det.sY = t.screenY;
+  },false);
+
+  ele.addEventListener('touchmove',function(e){
+  e.preventDefault();
+  var t = e.touches[0];
+  swipe_det.eX = t.screenX;
+  swipe_det.eY = t.screenY;
+  },false);
+
+  ele.addEventListener('touchend',function(e){
+  if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
+  if(swipe_det.eX > swipe_det.sX) direc = "next";
+  else direc = "prev";
+  }
+  if (direc != "") {
+    if(typeof func === 'function') func(el,direc);
+  }
+  direc = "";
+  },false);
+}
+
+let c_slider_m = 1;
+function swipeSliderMobile(el,d){
+  if(d === 'prev'){
+    c_slider_m++;
+    if(c_slider_m > 7) c_slider_m = 1;
+    sliderMobile(c_slider_m);
+  } else {
+    c_slider_m--;
+    if(c_slider_m < 1) c_slider_m = 7;
+    sliderMobile(c_slider_m);
+  }
+}
+
+function sliderMobile(c){
+  var img = _('#swipe_slider').children[0];
+  img.setAttribute('src', stringSliderMobile+c+'.jpg');
+}
+
 document.onkeydown = function(e){
   let wr = _('#popPhone');
   e = e || window.event;
